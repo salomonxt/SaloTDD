9eee660b6b7a5299e93635a5adf7f71103781431
salomonxt <salomonxt@protonmail.ch>
2025-08-25 15:05:12 +0200

    add

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -11,23 +11,22 @@ class MainTest {
            2. Zahl durch 5 teilbar gib " Buzz"
            3. Zahl durch 3 UND 5 teilbar gib "FizzBuzz"
            4. Zahl WEDER durch 3 ODER 5 teilbar gib Zahl zurück"
-           */
+        */
 
-
-
-//    void fizzBuzz_should
 
     //RED  - GREEN - REFRACTOR
     @Test
-    void fizzBuzz();
+    void fizzBuzz_shouldreturnFizz_whennumberis3()  {
 
-    /* void fizzBuzz_shouldReturnfizz_whenGivenNumer_is3() {
         //given
         String expected ="Fizz";
-        int n=3;
+        int n = 3;
         //when
         String actual = Main.fizzBuzz(n);
         //then
         assertEquals(expected, actual);
-    */
+
+    }
+
+
     }
